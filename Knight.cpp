67fc2ac893b18bccef12fc73a5114485654acbef
M-Ali05 <f250837@cfd.nu.edu.pk>
2026-05-10 16:20:39 +0500

    Refactor Knight class with position and move logic
    Refactor Knight class to include position and update move validation logic.

--- Knight.cpp
+++ Knight.cpp
@@ -1,22 +1,18 @@
 #include "Knight.h"
 #include <cstdlib>
-
-Knight::Knight(char c) :Piece(c) {}
-
-bool Knight::isWhite() { return color == 'W'; }
-
-char Knight::getSymbol() { return (color == 'W') ? 'N' : 'n'; }
-
-bool Knight::is_valid_move(int sx, int sy, int ex, int ey, Piece* board[8][8]) {
-
-    int dr = abs(ex - sx);
-    int dc = abs(ey - sy);
-
-    if (!((dr == 2 && dc == 1) || (dr == 1 && dc == 2)))
+Knight::Knight(char color, int row, int col) : Piece(color, row, col) { // Becaose inheriting from piece, initializer list is necessary
+}
+char Knight::get_symbol() {
+    return 'N'; // N is used becuase K is taken by King
+}
+bool Knight::is_valid_move(int to_row, int to_col, Piece* board[8][8]) {
+    int row_diff = abs(to_row - row);
+    int col_diff = abs(to_col - col);
+    if (!((row_diff == 2 && col_diff == 1) || (row_diff == 1 && col_diff == 2))) { // Knigt moves in L shape only
         return false;
-
-    if (board[ex][ey] && board[ex][ey]->isWhite() == isWhite())
-        return false;
-
-    return true;
+    }
+    if (board[to_row][to_col] != NULL && board[to_row][to_col]->get_color() == color) { // can't capture our own solider
+        return false; // through colour, solider can be recognized
+    }
+    return true; // means the solider of opponent will be arrested
 }
