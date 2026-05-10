8b1e2a43c05ab42d167217b14e15947ed6b82c63
M-Ali05 <f250837@cfd.nu.edu.pk>
2026-05-10 16:17:08 +0500

    Refactor King class for improved functionality
    Refactor King class to include row and column attributes and update movement validation.

--- King.cpp
+++ King.cpp
@@ -1,19 +1,16 @@
 #include "King.h"
 #include <cstdlib>
-
-King::King(char c) :Piece(c) {}
-
-bool King::isWhite() { return color == 'W'; }
-
-char King::getSymbol() { return (color == 'W') ? 'K' : 'k'; }
-
-bool King::is_valid_move(int sx, int sy, int ex, int ey, Piece* board[8][8]) {
-
-    if (abs(ex - sx) > 1 || abs(ey - sy) > 1)
+King::King(char color, int row, int col) : Piece(color, row, col) { // Becaose inheriting from piece, initializer list is necessary
+}
+char King::get_symbol() {
+    return 'K';
+}
+bool King::is_valid_move(int to_row, int to_col, Piece* board[8][8]) {
+    if (abs(to_row - row) > 1 || abs(to_col - col) > 1) { // King can only move one step in any direction
         return false;
-
-    if (board[ex][ey] && board[ex][ey]->isWhite() == isWhite())
-        return false;
-
-    return true;
+    }
+    if (board[to_row][to_col] != NULL && board[to_row][to_col]->get_color() == color) { // can't capture our own solider
+        return false; // through colour, solider can be recognized
+    }
+    return true; // means the solider of opponent will be arrested
 }
