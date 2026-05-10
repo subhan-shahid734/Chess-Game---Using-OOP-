f230ade797945d8776660e45177b1e54ea22935b
M-Ali05 <f250837@cfd.nu.edu.pk>
2026-05-10 16:26:44 +0500

    Fix formatting and remove unnecessary lines in Piece.h

--- Piece.h
+++ Piece.h
@@ -1,18 +1,16 @@
 #ifndef PIECE_H
 #define PIECE_H
-
 class Piece {
 protected:
     char color;
-
 public:
     Piece(char c);
     virtual ~Piece() {}
 
     virtual bool isWhite() = 0;
-    virtual char getSymbol() = 0;
+  virtual char getSymbol() = 0;
 
-    virtual bool is_valid_move(int sx, int sy, int ex, int ey, Piece* board[8][8]) = 0;
+       virtual bool is_valid_move(int sx, int sy, int ex, int ey, Piece* board[8][8]) = 0;
 };
 
 #endif
